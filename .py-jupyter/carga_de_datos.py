d979eb82539a8a6ac2bde825096cf5d24314e52a
bparedes21 <gestionorganizacion17@gmail.com>
2023-11-06 15:44:51 -0300

    ´prueba13

--- .py-jupyter/carga_de_datos.py
+++ .py-jupyter/carga_de_datos.py
@@ -6,12 +6,14 @@ import funciones_transformacion
 sucursal_df,productos_df,precios_semana_20200413_df,precios_20200419_20200419_df,precios_20200426_20200426_df,precios_semana_20200503_df,precios_semana_20200518_df=funciones_transformacion.transformar_todos_los_archivos()
 
 import sqlite3
+def conectar_db():
+    # Conectar a la base de datos (si no existe, se creará)
+    conn = sqlite3.connect('mi_base_de_datos.db')
+    return conn
 
-# Conectar a la base de datos (si no existe, se creará)
-conn = sqlite3.connect('mi_base_de_datos.db')
-
-# Crear un objeto cursor para ejecutar comandos SQL
+conn=conectar_db()
 cursor = conn.cursor()
+
 
 # Comando SQL para crear una tabla llamada 'sucursal' con dos columnas de IDs y una columna de precios
 cursor.execute('''DROP TABLE IF EXISTS sucursal;''')
@@ -29,25 +31,11 @@ CREATE TABLE sucursal (
     lng	TEXT
     sucursalNombre TEXT
     sucursalTipo TEXT
-    FOREIGN KEY(sucursal_id) REFERENCES sucursal(sucursal_id)
 );
 '''
+cursor.execute(create_table_query)
+conn.commit()
 print("Tabla 'sucursal' creada exitosamente.")
-
-#borrar tabla precio si existe
-cursor.execute('''DROP TABLE IF EXISTS precio;''')
-
-# Comando SQL para crear una tabla llamada 'precio' con dos columnas de IDs y una columna de precios
-create_table_query = '''
-CREATE TABLE precio (
-    sucursal_id TEXT NOT NULL,
-    producto_id INTEGER NOT NULL,
-    precio REAL,
-    FOREIGN KEY(sucursal_id) REFERENCES sucursal(sucursal_id),
-    FOREIGN KEY(producto_id) REFERENCES sucursal(producto_id)
-);
-'''
-print("Tabla 'precio' creada exitosamente.")
 
 #borrar tabla precio si existe
 cursor.execute('''DROP TABLE IF EXISTS producto;''')
@@ -64,11 +52,30 @@ CREATE TABLE producto (
     categoria3 TEXT
 );
 '''
+
 # Ejecutar el comando SQL para crear la tabla
 cursor.execute(create_table_query)
-
+conn.commit()
 # Guardar los cambios y cerrar la conexión
 print("Tabla 'producto' creada exitosamente.")
+
+#borrar tabla precio si existe
+cursor.execute('''DROP TABLE IF EXISTS precio;''')
+
+# Comando SQL para crear una tabla llamada 'precio' con dos columnas de IDs y una columna de precios
+create_table_query = '''
+CREATE TABLE precio (
+    sucursal_id TEXT NOT NULL,
+    producto_id INTEGER NOT NULL,
+    precio REAL,
+    FOREIGN KEY(sucursal_id) REFERENCES sucursal(sucursal_id),
+    FOREIGN KEY(producto_id) REFERENCES sucursal(producto_id)
+);
+'''
+cursor.execute(create_table_query)
+conn.commit()
+print("Tabla 'precio' creada exitosamente.")
+
 
 # Insertar o actualizar los datos en la tabla precio
 sucursal_df.to_sql('sucursal', conn, if_exists='replace', index=False, index_label='sucursal_id')
@@ -171,4 +178,6 @@ cursor.execute('SELECT COUNT(*) FROM precio')
 # Obtener el resultado de la consulta
 cantidad_registros = cursor.fetchone()[0]
 print("En la tabla precio se insertaron registros totales ",cantidad_registros)
+
+cursor.close()
 conn.close()
